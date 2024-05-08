68cc439aef80e62e1327691826a20f1275fbf8be
hamzabourass <roxmax224@gmail.com>
2024-05-08 19:30:16 +0100

    controller

--- src/main/java/com/example/secureapp/security/controllers/AccountRestController.java
+++ src/main/java/com/example/secureapp/security/controllers/AccountRestController.java
@@ -1,2 +1,42 @@
-package com.example.secureapp.security;public class AccountRestController {
+package com.example.secureapp.security.controllers;
+
+import com.example.secureapp.security.dtos.RoleUserForm;
+import com.example.secureapp.security.entities.AppRole;
+import com.example.secureapp.security.entities.AppUser;
+import com.example.secureapp.security.service.AccountService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AccountRestController {
+
+    private final AccountService accountService;
+
+    public AccountRestController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping(path="/users")
+    List<AppUser> listUsers(){
+        return accountService.listUsers();
+    }
+
+    @PostMapping(path = "/users")
+    AppUser saveUser(@RequestBody AppUser appUser){
+        return accountService.addNewUser(appUser);
+    }
+
+    @PostMapping(path = "/roles")
+    AppRole saveRole(@RequestBody AppRole appRole){
+        return accountService.addNewRole(appRole);
+    }
+
+    @PostMapping(path = "/addRoleToUser")
+    void addRoleToUser(@RequestBody RoleUserForm roleUserForm){
+        accountService.addRoleToUser(roleUserForm.getUsername(),roleUserForm.getRoleName());
+    }
 }
--- src/main/java/com/example/secureapp/security/controllers/AuthenticationController.java
+++ src/main/java/com/example/secureapp/security/controllers/AuthenticationController.java
@@ -1,2 +1,49 @@
-package com.example.secureapp.security.controllers;public class AuthenticationController {
+package com.example.secureapp.security.controllers;
+
+import com.example.secureapp.security.dtos.LoginResponse;
+import com.example.secureapp.security.dtos.LoginUserDto;
+import com.example.secureapp.security.dtos.RegisterUserDto;
+import com.example.secureapp.security.entities.AppRole;
+import com.example.secureapp.security.entities.AppUser;
+import com.example.secureapp.security.service.AccountService;
+import com.example.secureapp.security.service.AuthenticationService;
+import com.example.secureapp.security.service.JwtService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+@AllArgsConstructor
+public class AuthenticationController {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+    private final AccountService accountService;
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<AppUser> register(@RequestBody RegisterUserDto registerUserDto) {
+        AppUser registeredUser = authenticationService.signup(registerUserDto);
+        accountService.addRoleToUser(registeredUser.getEmail(),"USER");
+
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        AppUser authenticatedUser = authenticationService.authenticate(loginUserDto);
+
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+        String refreshToken = jwtService.refreshToken(authenticatedUser);
+
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setAccessToken(jwtToken);
+        loginResponse.setRefreshToken(refreshToken);
+        loginResponse.setExpiresIn(jwtService.getExpirationTime());
+
+        return ResponseEntity.ok(loginResponse);
+    }
 }
